8a2b2d1c4d1312fca79b0d184d83e9dd789edfd2
georg <leO/8?Rv>
2018-04-24 17:41:31 +0200

    sync commit for debugging

--- src/divergence.c
+++ src/divergence.c
@@ -43,6 +43,8 @@ void divergence(dat2d *lsmask, double *params, char **paths,
   if (DBGFLG>2) {printf("  divergence: calculate distances in grid\n");fflush(NULL);}
   // calculate distance grid
   for (nn=nymin; nn<(nymax-1); nn++){ // on staggered grid
+    printf("nymin: %d", nymin);fflush(NULL);
+    printf("nymax: %d", nymax);fflush(NULL);
     printf("nn: %d", nn);fflush(NULL);
     printf("lat[nn]: %f", lsmask->lat[nn]);fflush(NULL);
     printf("lat[nn]: %f", lsmask->lat[nn+1]);fflush(NULL);
